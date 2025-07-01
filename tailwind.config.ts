2cfbf4180c3320d322533df15ecab6a80f8a7137
hnndhr <hananadhira1475@gmail.com>
2025-07-01 16:20:01 +0700

    gatau apa yang salah

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,13 +2,30 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/app/**/*.{ts,tsx}",
-    "./src/components/**/*.{ts,tsx}",
-    "./src/pages/**/*.{ts,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Integrasi custom colors ke Tailwind
+        'darkest': '#623B1C',
+        'primary': '#744B28',
+        'secondary': '#422A3C',
+        'tertiary': '#4A3B30',
+        'accent': '#43CD31',
+        'light': '#CDA582',
+        'medium': '#937154',
+        'dark': '#936C4B',
+        'tan': '#D9B79A',
+        'cream': '#F1E1C7',
+        'peach': '#FFE3CB',
+        'ivory': '#FFEFD6',
+      },
+    },
   },
   plugins: [],
 };
+
 export default config;
